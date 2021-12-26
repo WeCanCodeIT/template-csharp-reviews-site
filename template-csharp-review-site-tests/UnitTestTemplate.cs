f1b8bb7b52cf560d517f868dfd16f2c8d74cd3a6
Carlos Lopez <carlos@wecancodeit.org>
2021-12-26 17:58:37 -0500

    initial commit

---
+++ template-csharp-review-site-tests/UnitTestTemplate.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace template_csharp_review_site_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
